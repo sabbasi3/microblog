a8428f110041537998d25094e1aadd80aaf1f3d1
sabbasi3 <13663848+sabbasi3@users.noreply.github.com>
2025-05-17 20:53:46 -0500

    make posts searchable with elastisearch

--- app/__init__.py
+++ app/__init__.py
@@ -36,6 +36,9 @@ def create_app(config_class=Config):
     mail.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
+
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+    if app.config['ELASTICSEARCH_URL'] else None
         
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
