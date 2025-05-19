6141b0261844385c2eb2ec329ae0dcb52e2caeaf
sabbasi3 <13663848+sabbasi3@users.noreply.github.com>
2025-05-19 18:05:17 -0500

    remove caching of senstive data for login

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,5 +2,10 @@ from flask import Blueprint
 
 bp = Blueprint('auth', __name__)
 
-from app.auth import routes
 
+@bp.after_request
+def add_cache_control(response):
+    response.headers['Cache-Control'] = 'no-store'
+    return response
+
+from app.auth import routes
