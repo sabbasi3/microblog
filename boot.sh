da7964bea258fdda23a2aeb2a0c01d6c2fa78a62
sabbasi3 <13663848+sabbasi3@users.noreply.github.com>
2025-05-19 22:48:21 -0500

    push latest changes in boot.sh to remote

--- boot.sh
+++ boot.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-flask db upgrade
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
 exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
 
 # This script is used to run the Flask application with Gunicorn. on docker image
+# add reties in case database is not ready to accept connections
